cba1d04c22c4e1e887c63bc2257c8cdc695a746a
ifun666 <67565745+ifun666@users.noreply.github.com>
2023-04-29 20:51:32 +0800

    Add files via upload

--- AutoSignIn.py
+++ AutoSignIn.py
@@ -1,10 +1,19 @@
-#!/usr/bin/env python
-# _*_ coding: utf-8 _*_
-
-import json
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import re
 import requests
+import json
+import yaml
 import time
+
+getToken_url = 'https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/login/we-chat/callback'
+getUserInfo_url = 'https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/user-api/course/last-info'
+getClass_url = 'https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/common-api/course/current'
+checkin_url = 'https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/user-api/course/join'
+
+headers = {
+    'Content-Type': 'text/plain'
+}
 
 session = requests.session()  # 对全局进行会话实例化
 
@@ -21,7 +30,6 @@ default_headers = {
     'Referer': 'https://qczj.h5yunban.com/qczj-youth-learning/mine.php',
     'Accept-Language': 'zh-CN,zh;q=0.9',
 }
-
 
 def get_resp(method, url, headers=default_headers, timeout=5, **kwargs):
     for i in range(5):  # 最多重试5次
@@ -42,63 +50,106 @@ def get_resp(method, url, headers=default_headers, timeout=5, **kwargs):
     return None
 
 
-def login():
-    while 1 == 1:
-        resp = get_resp('get', 'https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/login/we-chat/callback?callback=https%3A%2F%2Fqczj.h5yunban.com%2Fqczj-youth-learning%2Findex.php&scope=snsapi_userinfo&appid=wx56b888a1409a2920&openid=oO-a2txk3nyyiH81nkIUBiT6ZcSU&nickname=%25E6%259A%2597%25E9%25BB%2591%25E5%2590%258C%25E5%25AD%25A6&headimg=https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FVic3gRhvkNpgH3YHHiaO4IWk6zS6MicicaVgw3OxpiaFG6qtUhWsdrKZ5kDsiclDKFC9JO3L46mLSibAHrWpic7p3S4JdA%2F132&time=1668410297&source=common&sign=EF360516BBB0EBC9CBA6240419115FE3&t=1668410297')
-        print(resp)
-        accessToken = re.findall(r"'accessToken', '(.*?)'|$", str(resp))[0]
-        if accessToken != '':
-            print('登陆成功')
-            return accessToken
-        time.sleep(3)
+def getYmlConfig(yaml_file='config.yml'):
+    with open(yaml_file, 'r', encoding='utf-8') as f:
+        file_data = f.read()
+    return dict(yaml.load(file_data, Loader=yaml.FullLoader))
 
 
-def islogin(resp):
-    if 'login-typetip' in resp:
-        print('\n登录态失效，正在重新登陆！')
-        login()
-        return
+def getToken(openId):
+    # 根据openId获得token
+    try:
+        token = requests.get(url=getToken_url, params=openId, headers=headers)
+        Token_raw = token.text
+        Token = re.findall('[A-Z0-9]{8}[-][A-Z0-9]{4}[-][A-Z0-9]{4}[-][A-Z0-9]{4}[-][A-Z0-9]{12}', Token_raw)[0]
+        print('获取到Token为：' + Token)
+        return Token
+    except:
+        print('【致命错误】获取Token失败，请检查openId是否正确！')
+
+
+def getinfo(accessToken):
+    # 根据accessToken获得用户信息
+    try:
+        getUserInfo = requests.get(getUserInfo_url, params=accessToken, headers=headers)
+        userInfo = getUserInfo.json()
+        cardNo = userInfo["result"]["cardNo"]
+        nid = userInfo["result"]["nid"]
+        getClass = requests.get(getClass_url, params=accessToken, headers=headers)
+        Class = getClass.json()
+        classId = Class["result"]["id"]
+        classTitle = Class["result"]["title"]
+        infos: list = userInfo['result']['nodes']
+        Faculty = [item['title'] for item in infos]
+        print('本期大学习的课程编码：' + classId + '\n本期大学习的课程名称：' + classTitle + '\n系统中的个人信息为：' + cardNo + '\n您的签到所属组织为：' + str(Faculty))
+        checkinData = {
+            'course': classId,
+            'subOrg': None,
+            'nid': nid,
+            'cardNo': cardNo
+        }
+        return checkinData
+    except Exception as e:
+        if "is not subscriptable" in str(e):
+            print("【致命错误】openId出错,无法获得您的信息！")
+        print(f'【致命错误】获取历史信息失败，请您手动打卡：{e}')
+
+
+def signup(accessToken, checkinData):
+    # 根据token和data完成打卡
+    checkin = requests.post(checkin_url, params=accessToken, data=json.dumps(checkinData), headers=headers)
+    result = checkin.json()
+    if result["status"] == 200:
+        print("成功完成本次青年大学习！")
+        return 1
     else:
-        return
+        print('【致命错误】出现错误，错误码：' + str(result["status"]))
+        print('【致命错误】错误信息：' + str(result["message"]))
+        return result["message"]
 
 
-def main():
+if __name__ == "__main__":
     print("\n===============程序信息===============")
-    print("程序名称：青春浙江每日签到")
-    print("程序作者：浙中医的Richard同学")
-    print("更新时间：2022年11月8日")
-    print("程序版本：1.2.0_Beta")
+    print("程序名称：浙江青年大学习部署到Action一键打卡")
+    print("程序作者：lthero-big & 浙中医的Richard同学")
+    print("更新时间：2022年11月20日")
+    print("程序版本：1.0.0_Beta")
     print("===============信息结束===============")
+    config = getYmlConfig()
+    for index, eachuser in enumerate(config['users']):
+        print("\n===============单次开始===============")
+        print(eachuser['user']['name'] + 'openId为：' + eachuser['user']['openid'])
+        openid = {
+            'appid': 'wx56b888a1409a2920',
+            'openid': eachuser['user']['openid']
+        }
+        accessToken = getToken(openid)
 
-    accessToken = login()
-
-    params = {
-        'accessToken': accessToken,
-    }
-    json_data = {}
-    resp = get_resp('post', 'https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/user-api/sign-in', params=params, json=json_data)
-    print(resp)
-
-    params = {
-        'pageSize': '10',
-        'pageNum': '1',
-        'type': '百年风华正青春',
-        'desc': 'startTime',
-        'pid': 'C0096',
-    }
-    list_resp = get_resp('get', 'https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/common-api/course/list', params=params)
-    list_json = json.loads(list_resp)
-    for item in list_json['result']['list']:
-        # print(item['uriType'])
         params = {
             'accessToken': accessToken,
-            'id': item['id'],
         }
         json_data = {}
-        resp = get_resp('post', 'https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/user-api/course/study', params=params, json=json_data)
+        resp = get_resp('post', 'https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/user-api/sign-in', params=params, json=json_data)
         print(resp)
 
+        params = {
+            'pageSize': '10',
+            'pageNum': '1',
+            'type': '百年风华正青春',
+            'desc': 'startTime',
+            'pid': 'C0096',
+        }
+        list_resp = get_resp('get', 'https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/common-api/course/list', params=params)
+        list_json = json.loads(list_resp)
+        for item in list_json['result']['list']:
+            # print(item['uriType'])
+            params = {
+                'accessToken': accessToken,
+                'id': item['id'],
+            }
+            json_data = {}
+            resp = get_resp('post', 'https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/user-api/course/study', params=params, json=json_data)
+            print(resp)
 
-
-if __name__ == '__main__':
-    main()
+        print("===============单次结束===============")
+        time.sleep(3)
